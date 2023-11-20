9aac98776ddf9b049780d15e1f8f0ac3991247ee
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-11-20 22:38:17 +0530

    removing classes and styles

--- pages/index.js
+++ pages/index.js
@@ -18,188 +18,188 @@ export default function Home() {
     getListing(array);
   }
   
-  // return (
-  //   <div>
-  //       {listings.map((list, i) => (
-  //           <div key={i}> 
-  //             <div className="container mx-auto sm:px-4 px-4 py-5">
-  //               <h2 key={i} className="pb-2 border-b fw-bold">{list.Title}</h2>
-  //               <div className="flex flex-wrap  row-cols-1 row-cols-md-2 md:items-center g-5 py-5">
-  //                 <div className="relative flex-grow max-w-full flex-1 px-4 flex flex-col items-start gap-2">
-  //                   <div className="relative flex-grow max-w-full flex-1 px-4">
-  //                     <div className="relative flex flex-col min-w-0 rounded break-words border bg-white border-1 border-gray-300 overflow-hidden rounded-4 shadow-lg" >
-  //                       <div className="flex flex-col p-6 pb-0 text-shadow-1">
-  //                         <img className="overflow-hidden  rounded-4 shadow-lg" src={list.Picture + pinatajwt} style={{maxWidth:'500px', maxHeight:'500px'}}/>
-  //                         <ul className="flex list-unstyled mt-auto">
-  //                           <li className="flex items-center me-1 mt-1">
-  //                           <img src="map-solid.svg" alt="twbs" width="30" height="30" className="flex-shrink-0"/>
-  //                           </li>
-  //                           <li className="flex items-center me-2 mt-2">
-  //                             <h6 style={{color:'black'}}>{list.Address}</h6>
-  //                           </li>
-  //                           <li className="flex items-center me-1 mt-2">
-  //                             <h6 style={{color:'black'}}>{list.City}</h6>
-  //                           </li>
-  //                           <li className="flex items-center me-1 mt-2">
-  //                             <h6 style={{color:'black'}}>{list.City}</h6>
-  //                           </li>
-  //                           <li className="flex items-center me-1 mt-2">
-  //                             <h6 style={{color:'black'}}>{list.Country}</h6>
-  //                           </li>
-  //                           <li className="flex items-center me-1 mt-2">
-  //                             <h6 style={{color:'black'}}>{list.Zip}</h6>
-  //                           </li>
-  //                         </ul>
-  //                       </div>
-  //                     </div>
-  //                   </div>
-  //                   <div className="flex flex-wrap  row-cols-2 row-cols-md-2 ">
-  //                     <a className="relative block py-3 px-6 -mb-px border border-r-0 border-l-0 border-gray-300 no-underline w-fill flex gap-3 py-4" aria-current="true">
-  //                       <img src="dollar-solid.svg" alt="twbs" width="32" height="32" className="flex-shrink-0"/>
-  //                       <div className="flex gap-2 w-full justify-between">
-  //                         <div>
-  //                           <h5 className="mb-0">{list.Price}</h5>
-  //                         </div>
-  //                       </div>
-  //                     </a>
-  //                     <a className="relative block py-3 px-6 -mb-px border border-r-0 border-l-0 border-gray-300 no-underline w-fill flex gap-2 py-1" aria-current="true">
-  //                       <div className="flex gap-4 justify-between">
-  //                         <div>
-  //                           <h6 className="mb-0">Seller: {list.Name}<img src="person-solid.svg" alt="twbs" width="20" height="20" className="flex-shrink-0"/></h6>
-  //                         </div>
-  //                         <div>
-  //                           <h6 className="mb-0">{list.Email}<img src="at-solid.svg" alt="twbs" width="20" height="20" className="flex-shrink-0" style={{marginLeft:'2px'}}/></h6>
-  //                         </div>
-  //                         <div>
-  //                           <h6 className="mb-0">{list.Phone}<img src="phone-solid.svg" alt="twbs" width="20" height="20" className="flex-shrink-0" style={{marginLeft:'2px'}}/></h6>
-  //                         </div>
-  //                       </div>
-  //                       </a>
-  //                       <a className="relative block py-3 px-6 -mb-px border border-r-0 border-l-0 border-gray-300 no-underline w-fill flex gap-3 py-3" aria-current="true">
-  //                       <img src="calendar-solid.svg" alt="twbs" width="32" height="32" className="flex-shrink-0"/>
-  //                         <div className="flex gap-2 w-full justify-between">
-  //                           <div>
-  //                             <h5 className="mb-0">Listed since: {list.Listed}</h5>
-  //                           </div>
-  //                         </div>
-  //                       </a>
-  //                   </div>
-  //                 </div>
-  //                 <div className="relative flex-grow max-w-full flex-1 px-4">
-  //                   <div className="flex flex-wrap  row-cols-sm-2 g-4">
-  //           
-  //                   <div className="relative flex-grow max-w-full flex-1 px-4 flex flex-col gap-2">
-  //                     <div className="feature-icon-small inline-flex items-center  bg-gradient fs-4 rounded-3">
-  //                     <img src='house-solid.svg' width="32" height="32" style={{marginRight:'4px'}}/>
-  //                       <h4 className="fw-semibold">Floors {list.Floors}</h4>
-  //                       </div>
-  //                     </div>
-  //           
-  //                     <div className="relative flex-grow max-w-full flex-1 px-4 flex flex-col gap-2">
-  //                     <div className="feature-icon-small inline-flex items-center  bg-gradient fs-4 rounded-3">
-  //                     <img src='build-solid.svg' width="32" height="32" style={{marginRight:'4px'}}/>
-  //                       <h4 className="fw-semibold">Built {list.Year}</h4>
-  //                       </div>
-  //                     </div>
-  //           
-  //                     <div className="relative flex-grow max-w-full flex-1 px-4 flex flex-col gap-2">
-  //                       <div className="feature-icon-small inline-flex items-center  bg-gradient fs-4 rounded-3">
-  //                       <img src='bed-solid.svg' width="32" height="32" style={{marginRight:'4px'}}/>
-  //                       <h4 className="fw-semibold">Beds {list.Rooms}</h4>
-  //                       </div>
-  //                     </div>
-  //           
-  //                     <div className="relative flex-grow max-w-full flex-1 px-4 flex flex-col gap-2">
-  //                     <div className="feature-icon-small inline-flex items-center  bg-gradient fs-4 rounded-3">
-  //                     <img src='bath-solid.svg' width="32" height="32" style={{marginRight:'4px'}}/>
-  //                       <h4 className="fw-semibold">Baths {list.Baths}</h4>
-  //                       </div>
-  //                     </div>
-  //           
-  //                     <div className="relative flex-grow max-w-full flex-1 px-4 flex flex-col gap-2">
-  //                     <div className="feature-icon-small inline-flex items-center  bg-gradient fs-4 rounded-3">
-  //                     <img src='garage-solid.svg' width="32" height="32" style={{marginRight:'4px'}}/>
-  //                       <h4 className="fw-semibold">Garage {list.Garage}</h4>
-  //                       </div>
-  //                     </div>
-  //           
-  //                     <div className="relative flex-grow max-w-full flex-1 px-4 flex flex-col gap-2">
-  //                     <div className="feature-icon-small inline-flex items-center  bg-gradient fs-4 rounded-3">
-  //                     <img src='dollar-solid.svg' width="32" height="32" style={{marginRight:'4px'}}/>
-  //                       <h4 className="fw-semibold">HOA ${list.Hoa}</h4>
-  //                       </div>
-  //                     </div>
-  //                   </div>
-  //                   <div className="relative flex-grow max-w-full flex-1 px-4 flex flex-col py-5">
-  //                   <p>{list.Info}
-  //                   </p>
-  //                   </div>
-  //                 </div>
-  //               </div> 
-  //             </div>
-  //             <div className="b-example-divider"></div>
-  //           </div>
-  //       ))}
-  //   </div> 
-  // );
-  //
-  //
-  return(
-    <>
-      <div>
-        <div className='m-auto p-3'>
-          <h2 className='text-4xl font-bold p-4 m-4'>Title</h2>
-        </div>
-        <div className='flex flex-row'>
-          <div>
-            <div placeholder='image section' className='p-6 m-4'> 
+  return (
+    <div>
+        {/* {listings.map((list, i) => ( */}
+            <div> 
               <div>
-                <img placeholder='room image'/> Image
-              </div>
+                <h2>Title</h2>
+                <div>
+                  <div>
+                    <div>
+                      <div>
+                        <div>
+                          {/* <img src={Picture + pinatajwt} /> */}
+                          <ul>
+                            <li >
+                            <img src="map-solid.svg" alt="twbs" width="30" height="30" className=""/>
+                            </li>
+                            <li className="">
+                              <h6>Address</h6>
+                            </li>
+                            <li className="">
+                              <h6>City</h6>
+                            </li>
+                            <li className="">
+                              <h6>City</h6>
+                            </li>
+                            <li className="">
+                              <h6> Country</h6>
+                            </li>
+                            <li className="">
+                              <h6>Zip</h6>
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="">
+                      <a className="" aria-current="true">
+                        <img src="dollar-solid.svg" alt="twbs" width="32" height="32" className=""/>
+                        <div className="">
+                          <div>
+                            <h5 className="">Price</h5>
+                          </div>
+                        </div>
+                      </a>
+                      <a className="" aria-current="true">
+                        <div className="">
+                          <div>
+                            <h6 className="">Seller: Name<img src="person-solid.svg" alt="twbs" width="20" height="20" className=""/></h6>
+                          </div>
+                          <div>
+                            <h6 className="">Email<img src="at-solid.svg" alt="twbs" width="20" height="20" className="" /></h6>
+                          </div>
+                          <div>
+                            <h6 className="">Phone<img src="phone-solid.svg" alt="twbs" width="20" height="20" className="" /></h6>
+                          </div>
+                        </div>
+                        </a>
+                        <a className="" aria-current="true">
+                        <img src="calendar-solid.svg" alt="twbs" width="32" height="32" className=""/>
+                          <div className="flex gap-2 w-full justify-between">
+                            <div>
+                              <h5 className="">Listed since: Listed</h5>
+                            </div>
+                          </div>
+                        </a>
+                    </div>
+                  </div>
+                  <div className="">
+                    <div className="">
             
-              <div className='m-2 p-2'>
-                <ul className='flex flex-row my-2'>
-                  <li className='p-2'>Icon</li>
-                  <li className='p-2'>Place</li>
-                  <li className='p-2'>Country</li>
-                </ul>
+                    <div className="">
+                      <div className="">
+                      <img src='house-solid.svg' width="32" height="32" />
+                        <h4 className="">Floors Floors</h4>
+                        </div>
+                      </div>
+            
+                      <div className="">
+                      <div className="">
+                      <img src='build-solid.svg' width="32" height="32" />
+                        <h4 className="fw-semibold">Built Year</h4>
+                        </div>
+                      </div>
+            
+                      <div className="">
+                        <div className="">
+                        <img src='bed-solid.svg' width="32" height="32" />
+                        <h4 className="fw-semibold">Beds Rooms</h4>
+                        </div>
+                      </div>
+            
+                      <div className="">
+                      <div className="">
+                      <img src='bath-solid.svg' width="32" height="32" />
+                        <h4 className="fw-semibold">Baths Baths</h4>
+                        </div>
+                      </div>
+            
+                      <div className="">
+                      <div className="">
+                      <img src='garage-solid.svg' width="32" height="32" />
+                        <h4 className="fw-semibold">Garage Garage</h4>
+                        </div>
+                      </div>
+            
+                      <div className="">
+                      <div className="">
+                      <img src='dollar-solid.svg' width="32" height="32" />
+                        <h4 className="fw-semibold">HOA Hoa</h4>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="">
+                    <p>Info
+                    </p>
+                    </div>
+                  </div>
+                </div> 
               </div>
+              <div className=""></div>
             </div>
-        
-            <div placeholder='price section' className='m-4 p-6'>
-              <span placeholder='price' /> Price
-              <ul placeholder='Address' className='flex flex-row align-middle'>
-                <li className='p-2'>Seller</li>
-                <li className='p-2'>email</li>
-                <li className='p-2'>No</li>
-              </ul>
-              <span placeholder='listing date' /> Listing Date
-            </div>
-          </div>
-        
-          <div placeholder='info section' className='m-4 p-6'>
-            <span className='flex-grow max-w-full flex-1 px-4 flex flex-col gap-2'>
-              Floor
-            </span>          
-            <span className='flex-grow max-w-full flex-1 px-4 flex flex-col gap-2'>
-              Beds
-            </span>          
-            <span className='flex-grow max-w-full flex-1 px-4 flex flex-col gap-2'>
-              Garage
-            </span>          
-            <span className='flex-grow max-w-full flex-1 px-4 flex flex-col gap-2'>
-              Built
-            </span>          
-            <span className='flex-grow max-w-full flex-1 px-4 flex flex-col gap-2'>
-              Bath
-            </span>          
-            <span className='flex-grow max-w-full flex-1 px-4 flex flex-col gap-2'>
-              HOA
-            </span>          
-            <p className='m-4 p-6'>Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.</p>
-          </div>
-        </div>
-      </div>
-    </>
+        {/* ))} */}
+    </div> 
   );
+  //
+  //
+  // return(
+  //   <>
+  //     <div>
+  //       <div className='m-auto p-3'>
+  //         <h2 className='text-4xl font-bold p-4 m-4'>Title</h2>
+  //       </div>
+  //       <div className='flex flex-row'>
+  //         <div>
+  //           <div placeholder='image section' className='p-6 m-4'> 
+  //             <div>
+  //               <img placeholder='room image'/> Image
+  //             </div>
+  //           
+  //             <div className='m-2 p-2'>
+  //               <ul className='flex flex-row my-2'>
+  //                 <li className='p-2'>Icon</li>
+  //                 <li className='p-2'>Place</li>
+  //                 <li className='p-2'>Country</li>
+  //               </ul>
+  //             </div>
+  //           </div>
+  //       
+  //           <div placeholder='price section' className='m-4 p-6'>
+  //             <span placeholder='price' /> Price
+  //             <ul placeholder='Address' className='flex flex-row align-middle'>
+  //               <li className='p-2'>Seller</li>
+  //               <li className='p-2'>email</li>
+  //               <li className='p-2'>No</li>
+  //             </ul>
+  //             <span placeholder='listing date' /> Listing Date
+  //           </div>
+  //         </div>
+  //       
+  //         <div placeholder='info section' className='m-4 p-6'>
+  //           <span className='flex-grow max-w-full flex-1 px-4 flex flex-col gap-2'>
+  //             Floor
+  //           </span>          
+  //           <span className='flex-grow max-w-full flex-1 px-4 flex flex-col gap-2'>
+  //             Beds
+  //           </span>          
+  //           <span className='flex-grow max-w-full flex-1 px-4 flex flex-col gap-2'>
+  //             Garage
+  //           </span>          
+  //           <span className='flex-grow max-w-full flex-1 px-4 flex flex-col gap-2'>
+  //             Built
+  //           </span>          
+  //           <span className='flex-grow max-w-full flex-1 px-4 flex flex-col gap-2'>
+  //             Bath
+  //           </span>          
+  //           <span className='flex-grow max-w-full flex-1 px-4 flex flex-col gap-2'>
+  //             HOA
+  //           </span>          
+  //           <p className='m-4 p-6'>Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.</p>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </>
+  // );
 }
